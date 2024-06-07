061e7601e1da181cf37ba8e4e3a6332bf91e0f21
kumo <124721166+kumoSleeping@users.noreply.github.com>
2024-06-08 00:22:00 +0800

    ✨ update: 适配新版 Chat Tsugu Py (#2)
    * ✨ update files✨
    
    * ✨ update files✨
    
    * Update pyproject.toml

--- nonebot_plugin_tsugu_frontend/config.py
+++ nonebot_plugin_tsugu_frontend/config.py
@@ -55,33 +55,3 @@ class Config(BaseModel):
     是否压缩返回数据，压缩可减少返回数据大小。
     默认为 True，即压缩返回数据。若不压缩返回数据，可将此项设置为 False。
     '''
-
-    tsugu_prefix: List[str] = ['/', '']
-    '''
-    命令前缀
-    最后一个参数如果不是空字符串，则只有在命令前缀符合时才会触发命令。
-    '''
-
-    tsugu_allow_gap_less: bool = True
-    '''
-    是否允许命令与参数之间没有空格
-    '''
-
-    tsugu_get_remote_user_data_max_retry: int = 3
-    '''
-    获取远程用户数据最大重试次数
-    '''
-
-    tsugu_token_name: str = "Tsugu"
-    '''
-    bandori station token
-    '''
-    tsugu_bandori_station_token: str = "ZtV4EX2K9Onb"
-    '''
-    bandori station token
-    '''
-
-    tsugu_ban_gacha_simulate_group_data: List = []
-    '''
-    需要关闭模拟抽卡的群
-    '''
